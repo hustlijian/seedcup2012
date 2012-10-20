903e43b74112d0f91bbb296f269c49cf63a51e90
hzhigeng@gmail.com <hzhigeng@gmail.com@ff23b933-8c70-6461-5132-2e3ee6bb378f>
2012-10-20 11:33:21 +0000

    好吧，我疏忽了，事实证明，以后还是先编译运行成功后再上传代码。。修正DatabaseAPI.h的rename函数，现拆分为2个函数。

--- Src/DatabaseAPI.h
+++ Src/DatabaseAPI.h
@@ -46,7 +46,8 @@ int alterColumn(char *tableName, char *columnName, COLUMN_TYPE newColumnType);
 int truncateTable(char *tableName);
 int use(char *databaseName);
 int drop(char *databaseName, char *tableName); 	//tableName为NULL时删除整个database
-int rename(char *oldName, char newName, int renameTable); //1为重命名表，0为重命名数据库
+int renameDatabase(char *oldName, char *newName);
+int renameTable(char *oldName, char *newName);
 
 //select函数接口可能需要重写，由于Select可能产生多行数据，每行数据即是一个Value*数组，
 //所以我考虑返回Value*型的二维数组，其中rowAmount指针指向的值记录了行数
