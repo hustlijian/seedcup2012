995151312d9feb18bf19036030cfec9a2c5fe8e4
hustlijian@gmail.com <hustlijian@gmail.com@ff23b933-8c70-6461-5132-2e3ee6bb378f>
2012-10-20 15:41:15 +0000

    实现了分词功能，种别码见参考文档

---
+++ trunk/Src/CmdProcess.h
@@ -0,0 +1,14 @@
+#ifndef CMDPROCESS_H
+#define CMDPROCESS_H
+
+#define NUM_KEYWORDS 33
+#define MAX_KEYWORDS_LENGTH 10
+#define NUM_OPERATOR	19
+#define MAX_OPERATOR	3
+#define NUM_SPACE		3
+#define MAX_WORDLEGTH  1024
+
+/*在返回值为int的函数中，返回-1代表失败，返回0代表成功*/
+int processCmd(char *cmd);
+void test();
+#endif //CMDPROCESS_H
