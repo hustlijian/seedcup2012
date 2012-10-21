21b7561353bbff014bcac48223daddb06623c2e3
hustlijian@gmail.com <hustlijian@gmail.com@ff23b933-8c70-6461-5132-2e3ee6bb378f>
2012-10-21 14:55:10 +0000

    实现了数据库定义，显示的命令解析。测试过，调用成功。 TODO:数据操作的命令未解析：select, update, delete,insert,

--- Src/CmdProcess.h
+++ Src/CmdProcess.h
@@ -3,11 +3,11 @@
 
 #define NUM_KEYWORDS 33
 #define MAX_KEYWORDS_LENGTH 10
-#define NUM_OPERATOR	19
+#define NUM_OPERATOR	20
 #define MAX_OPERATOR	3
 #define NUM_SPACE		3
 #define MAX_WORDLEGTH  1024
 
+/*在返回值为int的函数中，返回-1代表失败，返回0代表成功*/
 int processCmd(char *cmd);
-
 #endif //CMDPROCESS_H
