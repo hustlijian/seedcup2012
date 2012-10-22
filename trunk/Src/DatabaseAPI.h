a17d857c87b8018ee85cfc1d34a2d7854de85217
hustlijian@gmail.com <hustlijian@gmail.com@ff23b933-8c70-6461-5132-2e3ee6bb378f>
2012-10-22 12:16:25 +0000

    添加了insert, delete, update的命令解析， 修改了API，添加了一个数据类型表示空

--- trunk/Src/DatabaseAPI.h
+++ trunk/Src/DatabaseAPI.h
@@ -1,7 +1,7 @@
 #ifndef DATABASEAPI_H_INCLUDED
 #define DATABASEAPI_H_INCLUDED
 
-typedef enum {INT, FLOAT, TEXT, NONE} COLUMN_TYPE;
+typedef enum {INT, FLOAT, TEXT, NONE, EMPTY} COLUMN_TYPE;
 typedef enum {EQ, NE, GT, LT, GET, LET, BETWEEN, LIKE} OPERATOR;
 typedef enum {NOTSORT, DESC, INCR} SORT_ORDER;
 
@@ -57,10 +57,9 @@ int update(UpdateBody *updateBOdy);
 int delete(char *tableName, char *column, Value value);
 int insert(char *tableName, char **columnsName, Value *values, int amount);
 
-void showDatabase(SORT_ORDER sortOrder);
+int showDatabase(SORT_ORDER sortOrder);
 int showTable(char *databaseName,SORT_ORDER sortOrder);  //databaseName为NULL时指定为当前数据库
 int showColumn(char *tableName, SORT_ORDER sortOrder);
-int showColumnType(char *tableName);
-int showAllColumnValue(char *tableName);
+int showColumnValue(char *tableName);
 
 #endif // DATABASEAPI_H_INCLUDED
